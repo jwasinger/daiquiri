03e8e575d289a77abebd8a3d5c4802f3df8988f3
Jared Wasinger <j-wasinger@hotmail.com>
2020-02-17 22:11:50 +0000

    add bn support

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var assert = require("assert");
 var fs = require("fs");
 const js_yaml = require("js-yaml");
+const BN = require('bn.js')
+const { TWO_POW256 } = require('ethereumjs-util')
 
 let res = null;
 let mem = null;
@@ -16,6 +18,45 @@ var memset = function(mem, offset, data) {
 var memget = function(mem, offset, length) {
   return Buffer.from(new Uint8Array(mem.buffer, offset, length));
 };
+
+// 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47
+var bn128_field_modulus = new BN('30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47', 16);
+var bn128_r_inv = new BN('9ede7d651eca6ac987d20782e4866389', 16);
+var bn128_r_squared = new BN('06d89f71cab8351f47ab1eff0a417ff6b5e71911d44501fbf32cfc5b538afa89', 16)
+
+const MASK_256 = new BN('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 16);
+
+var field_modulus = bn128_field_modulus;
+var r_inv = bn128_r_inv;
+var r_squared = bn128_r_squared;
+
+function addmod(a, b) {
+  var res = a.add(b);
+  if (res.cmp(field_modulus) >= 0) {
+    res.isub(field_modulus);
+  }
+  return res
+}
+
+function submod(a, b) {
+  var res = a.sub(b);
+  if (res.cmpn(0) < 0) {
+    res.iadd(field_modulus);
+  }
+  return res
+}
+
+function mulmodmont(a, b) {
+  var t = a.mul(b);
+  var k0 = t.mul(r_inv).maskn(128);
+  var res2 = k0.mul(field_modulus).add(t).shrn(128);
+  var k1 = res2.mul(r_inv).maskn(128);
+  var result = k1.mul(field_modulus).add(res2).shrn(128);
+  if (result.gt(field_modulus)) {
+    result = result.sub(field_modulus)
+  }
+  return result
+}
 
 function getImports(env) {
   return {
@@ -39,6 +80,152 @@ function getImports(env) {
       },
       eth2_blockDataSize: function() {
         return env.blockData.byteLength;
+      },
+      bignum_add256: (aOffset, bOffset, cOffset) => {
+        const a = new BN(memget(mem, aOffset, 32))
+        const b = new BN(memget(mem, bOffset, 32))
+        const c = a.add(b).mod(TWO_POW256).toArrayLike(Buffer, 'be', 32)
+        memset(mem, cOffset, c)
+      },
+      bignum_mul256: (aOffset, bOffset, cOffset) => {
+        const a = new BN(memget(mem, aOffset, 32))
+        const b = new BN(memget(mem, bOffset, 32))
+        const c = a.mul(b).mod(TWO_POW256).toArrayLike(Buffer, 'be', 32)
+        memset(mem, cOffset, c)
+      },
+      bignum_sub256: (aOffset, bOffset, cOffset) => {
+        const a = new BN(memget(mem, aOffset, 32))
+        const b = new BN(memget(mem, bOffset, 32))
+        const c = a.sub(b).toTwos(256).toArrayLike(Buffer, 'be', 32)
+        memset(mem, cOffset, c)
+      },
+      bignum_div256: (aOffset, bOffset, cOffset) => {
+        const a = new BN(memget(mem, aOffset, 32))
+        const b = new BN(memget(mem, bOffset, 32))
+        if (b.isZero()) throw new Error('division by zero')
+        const c = a.div(b).toArrayLike(Buffer, 'be', 32)
+        memset(mem, cOffset, c)
+      },
+      bignum_mulMod: (aOffset, bOffset, cOffset, rOffset) => {
+        const a = new BN(memget(mem, aOffset, 32))
+        const b = new BN(memget(mem, bOffset, 32))
+        const c = new BN(memget(mem, cOffset, 32))
+        if (c.isZero()) throw new Error('modulus is zero')
+        const r = a.mul(b).mod(c).toArrayLike(Buffer, 'be', 32)
+        memset(mem, rOffset, r)
+      },
+      // modular multiplication of two numbers in montgomery form (i.e. montgomery multiplication)
+      bignum_f1m_mul: (aOffset, bOffset, rOffset) => {
+        const a = new BN(memget(mem, aOffset, 32), 'le')
+        const b = new BN(memget(mem, bOffset, 32), 'le')
+
+        var result = mulmodmont(a, b);
+
+        //console.log('bignum_f1m_mul a:', a.toString())
+        //console.log('bignum_f1m_mul b:', b.toString())
+        //console.log('bignum_f1m_mul result:', result.toString())
+
+        var result_le = result.toArrayLike(Buffer, 'le', 32);
+
+        memset(mem, rOffset, result_le)
+      },
+      bignum_f1m_square: (inOffset, outOffset) => {
+        const in_param = new BN(memget(mem, inOffset, 32), 'le');
+        var result = mulmodmont(in_param, in_param);
+
+        var result_le = result.toArrayLike(Buffer, 'le', 32)
+        memset(mem, outOffset, result_le)
+      },
+      bignum_f1m_add: (aOffset, bOffset, outOffset) => {
+        const a = new BN(memget(mem, aOffset, 32), 'le');
+        const b = new BN(memget(mem, bOffset, 32), 'le');
+        var result = addmod(a, b);
+
+        var result_le = result.toArrayLike(Buffer, 'le', 32)
+
+        memset(mem, outOffset, result_le)
+      },
+      bignum_f1m_sub: (aOffset, bOffset, outOffset) => {
+        const a = new BN(memget(mem, aOffset, 32), 'le');
+        const b = new BN(memget(mem, bOffset, 32), 'le');
+        var result = submod(a, b);
+
+        var result_le = result.toArrayLike(Buffer, 'le', 32)
+        memset(mem, outOffset, result_le)
+      },
+      bignum_f1m_toMontgomery: (inOffset, outOffset) => {
+        const in_param = new BN(memget(mem, inOffset, 32), 'le');
+
+        var result = mulmodmont(in_param, r_squared);
+        var result_le = result.toArrayLike(Buffer, 'le', 32)
+
+        memset(mem, outOffset, result_le)
+      },
+      bignum_f1m_fromMontgomery: (inOffset, outOffset) => {
+        const in_param = new BN(memget(mem, inOffset, 32), 'le');
+
+        var one = new BN('1', 16);
+        var result = mulmodmont(in_param, one);
+        var result_le = result.toArrayLike(Buffer, 'le', 32)
+
+        memset(mem, outOffset, result_le)
+      },
+      bignum_int_add: (aOffset, bOffset, outOffset) => {
+        const a = new BN(memget(mem, aOffset, 32), 'le');
+        const b = new BN(memget(mem, bOffset, 32), 'le');
+        //const result = a.add(b).maskn(256);
+
+        // websnark int_add returns a carry bit if the operation overflowed
+        const resultFull = a.add(b);
+        let carry = 0;
+        if (resultFull.gt(MASK_256)) {
+          carry = 1;
+        }
+        //const result = resultFull.maskn(256);
+        const result = resultFull.mod(TWO_POW256); // how ethereumjs-vm does it
+        const result_le = result.toArrayLike(Buffer, 'le', 32)
+        memset(mem, outOffset, result_le)
+
+        return carry
+      },
+      bignum_int_sub: (aOffset, bOffset, outOffset) => {
+        const a = new BN(memget(mem, aOffset, 32), 'le')
+        const b = new BN(memget(mem, bOffset, 32), 'le')
+
+        // websnark int_sub returns a carry bit
+        const resultFull = a.sub(b)
+        let carry = 0
+        if (resultFull.isNeg()) {
+          carry = 1
+        }
+        const result = resultFull.toTwos(256);
+
+        const result_le = result.toArrayLike(Buffer, 'le', 32)
+        memset(mem, outOffset, result_le)
+        
+        return carry
+      },
+      bignum_int_mul: (aOffset, bOffset, outOffset) => {
+        const a = new BN(memget(mem, aOffset, 32), 'le');
+        const b = new BN(memget(mem, bOffset, 32), 'le');
+        //const result = a.mul(b).maskn(256);
+        const result = a.mul(b).mod(TWO_POW256);
+
+        const result_le = result.toArrayLike(Buffer, 'le', 32)
+        memset(mem, outOffset, result_le)
+      },
+      bignum_int_div: (aOffset, bOffset, cOffset, rOffset) => {
+        // c is the quotient
+        // r is the remainder
+        const a = new BN(memget(mem, aOffset, 32), 'le');
+        const b = new BN(memget(mem, bOffset, 32), 'le');
+        // @ts-ignore
+        const result = a.divmod(b);
+        const result_quotient_le = result.div.toArrayLike(Buffer, 'le', 32)
+        const result_remainder_le = result.mod.toArrayLike(Buffer, 'le', 32)
+
+        memset(mem, cOffset, result_quotient_le)
+        memset(mem, rOffset, result_remainder_le)
       }
     }
   };
